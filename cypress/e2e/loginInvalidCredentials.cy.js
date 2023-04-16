fd1f845de61b5d2d3e53cb91f662a42200da3a77
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-04-16 20:18:19 +0200

    Adds cypress install and e2e testing to project

---
+++ cypress/e2e/loginInvalidCredentials.cy.js
@@ -0,0 +1,31 @@
+import { userName } from "../../cypress.env.js";
+
+describe("Login using wrong password to get error message", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.clearLocalStorage();
+    cy.wait(1000);
+  });
+
+  it("Fails using of invalid password", () => {
+    //triggers login form
+    cy.get("#registerForm button")
+      .contains("Login")
+      .should("be.visible")
+      .click();
+    cy.wait(1000);
+
+    //log in with wrong credentials/password
+    cy.get("input#loginEmail").type(userName);
+    cy.get("input#loginPassword").type("wrongCredential", { delay: 200 });
+    cy.get("#loginForm button").contains("Login").click();
+    cy.wait(1000);
+
+    //gets message in cypress with assert expected, shows a status 404
+    cy.on("window:alert", (message) => {
+      expect(message).to.eq(
+        "Either your username was not found or your password is incorrect"
+      );
+    });
+  });
+});
